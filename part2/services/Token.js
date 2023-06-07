8137665b97b41391dfc6e39cfb9b74d56b0b6dd8
kalamam <110126199+kalamam@users.noreply.github.com>
2023-06-07 21:03:26 +0300

    Delete part2 directory

--- part2/services/Token.js
+++
@@ -1,8 +0,0 @@
-const Token = require('..models/Token')
-
-const createToken = async (username, password) => {
-    const token = new Token({username, password});
-    return await token.save();
-}
-
-module.exports = { createToken }
