c97edb9ff6ba58f1bf142fb91a6732d744a38d94
kalamam <110126199+kalamam@users.noreply.github.com>
2023-06-25 19:39:52 +0300

    build repositories

---
+++ android-client/app/build/generated/source/buildConfig/debug/com/example/manishma/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.manishma;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.makore";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
