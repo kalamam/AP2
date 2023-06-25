e764086979af6449f920861830a7a2595a49e230
noamKap1 <110221265+noamKap1@users.noreply.github.com>
2023-06-25 20:03:52 +0300

    Delete android-client/app/src/main/java/com/example/manishma/chat directory

--- android-client/app/src/main/java/com/example/manishma/chat/ContactClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.manishma.chat;
-
-import android.view.View;
-
-import com.example.manishma.entities.Contact;
-
-public interface ContactClickListener {
-    void onContactClick(View v, Contact contact);
-}
